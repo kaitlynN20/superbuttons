988b879ec15984dce2726057990f9618f5c3c352
Kaity <kaitlynn281@gmail.com>
2018-02-13 17:01:38 -0500

    made a cool button and added alert

--- superbuttons/ViewController.swift
+++ superbuttons/ViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var toggleFlag: Bool = true
+    
+    @IBOutlet weak var humansBttn: UIButton!
+   
+    @IBAction func humansFUN(_ sender: Any) {
+        if (toggleFlag == true) {
+            humansBttn.tintColor = UIColor.init(named: "forgrd")
+            humansBttn.setBackgroundImage(UIImage(named:"People"), for: .normal)
+            toggleFlag = false
+        } else {
+            humansBttn.tintColor = UIColor.init(named:"accent")
+            humansBttn.setBackgroundImage(UIImage(named: "Two"), for: .normal)
+            toggleFlag = true
+        }
+        
+        let myAlert = UIAlertController(title: "Floor Prefernece", message: "My living room florr is?", preferredStyle: .alert)
+        
+        myAlert.addAction(UIAlertAction(title:"Carpet", style: .default,handler: nil))
+        myAlert.addAction(UIAlertAction(title:"Wood", style: .cancel, handler: {
+            
+            ACTION in
+            print ("User picked wood")
+        }))
+        
+        self.present(myAlert, animated: true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
